7fb774b17d64e7c9a49b6db9c6b865c7fab7a98c
Dannie15 <99117623+Dannie15@users.noreply.github.com>
2022-03-02 08:39:45 +0100

    Add files via upload

---
+++ start.js
@@ -0,0 +1,4 @@
+ var info = prompt("What's your name");
+ var date =  new Date().getFullYear();
+ document.getElementById("dot").innerHTML = "copyright" + " " +  date ;
+document.querySelector("h1").innerHTML= " Hi " + info + " welcome to my website ";
